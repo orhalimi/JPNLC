1013152244780411e8997225e612626066f32d28
Or Halimi <or50002001@gmail.com>
2017-10-18 00:35:47 +0300

    css modules are not for now...

--- app/components/checkBox.jsx
+++ app/components/checkBox.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import css from '../css/main.css';
 
 const CheckBox = props => {
-  return <h1 className={css.c}>1234</h1>//<input type="checkbox" className="c" />;
+  return <h1 className="c">1234</h1>//<input type="checkbox" className="c" />;
 };
 
 export default CheckBox
--- webpack.dev.js
+++ webpack.dev.js
@@ -6,14 +6,7 @@ module.exports = merge(common, {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true,
-              localIdentName: '[path][name]__[local]--[hash:base64:5]',
-            },
-          }],
+        use: ['style-loader', 'css-loader'],
       },
     ],
   },
