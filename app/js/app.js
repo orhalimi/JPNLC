a6fdc4eb9bd4cf91a85d656012f694aaa1676d34
Or Halimi <or50002001@gmail.com>
2017-10-26 20:28:12 +0300

    database demo

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,9 @@
+/*
+Author: Or Halimi
+
+Copyright (c) 2017 Or Halimi. All rights reserved.
+*/
+
 
 import { init } from 'app/controller';
 import css from 'css/main.css';
--- app/js/data.js
+++ app/js/data.js
@@ -3,18 +3,60 @@ const CONST = {
   conjBase: 'base',
   conjTrans: 'transaction',
   conjBoth: 'both',
-
+  wordType: {
+    ruVerb: 'ru verb',
+    uVerb: 'u verb',
+    suruVerb: 'suru verb',
+    kuruVerb: 'kuru verb',
+  },
+  conjuctionType: {
+    masu: 'masu',
+    te: 'te',
+    dictionary: 'dictionary',
+  },
 };
-
+const {
+  ruVerb, uVerb, suruVerb, kuruVerb,
+} = CONST.wordType;
+const { conjuctionType } = CONST;
 
 const conjForms = [
-  { id: 'masu', text: 'masu form', use: CONST.conjBoth },
-  { id: 'te', text: 'Te form', use: CONST.conjBoth },
-  { id: 'dictionary', text: 'dictionary form', use: CONST.conjBoth },
+  { id: conjuctionType.masu, text: 'masu form', use: CONST.conjBoth },
+  { id: conjuctionType.te, text: 'Te form', use: CONST.conjBoth },
+  { id: conjuctionType.dictionary, text: 'dictionary form', use: CONST.conjBoth },
 ];
 
+const words = [
+  {
+    dictionary: '食べる',
+    hiragana: 'たべる',
+    type: ruVerb,
+    translate: 'to eat',
+    missing: [conjuctionType.te],
+    exeptions: { masu: 'たべまます' },
+  },
+  {
+    dictionary: '聞く',
+    hiragana: 'きく',
+    translate: 'to hear; to ask',
+    type: uVerb,
+  },
+  {
+    dictionary: 'する',
+    hiragana: 'する',
+    translate: 'to do',
+    type: suruVerb,
+  },
+  {
+    dictionary: '来る',
+    hiragana: 'くる',
+    translate: 'to go',
+    type: kuruVerb,
+  },
+];
 
 export {
   CONST,
   conjForms,
+  words,
 };
