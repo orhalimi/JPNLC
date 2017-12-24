694946e9174914e06c77f32632c0aacb6b032d72
Or Halimi <or50002001@gmail.com>
2017-12-24 22:09:31 +0200

    update git ignore

--- webpack.common.js
+++ webpack.common.js
@@ -30,15 +30,11 @@ module.exports = {
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
-        use: [
-          'file-loader',
-        ],
+        use: ['file-loader'],
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: [
-          'file-loader',
-        ],
+        use: ['file-loader'],
       },
     ],
   },
@@ -46,8 +42,5 @@ module.exports = {
     filename: '[name].bundle.js',
     path: `${__dirname}/build`,
   },
-  plugins: [
-    HTMLWebpackPluginConfig,
-    new CleanWebpackPlugin(['build']),
-  ],
+  plugins: [HTMLWebpackPluginConfig, new CleanWebpackPlugin(['build'])],
 };
